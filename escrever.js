1802f5eeafa1ac808f7098d881ad1d4df30be9b7
jnpefo <jnpefo@gmail.com>
2021-09-21 20:41:43 -0300

    acho que achamos uma soluçao

--- escrever.js
+++ escrever.js
@@ -1,43 +1,39 @@
 const button = document.querySelector('#button')
 const text = document.querySelector('#text')
+const text2 = document.querySelector('#text2')
 
 const recognition = creatRecognition()
 let listening = false;
 
+function escreve() {
+  recognition.start;
+}
 button.addEventListener('click', e => {
   if(!recognition) return;
-
-  listening ? recognition.stop() : recognition.start()
+  
+  listening
+    ? recognition.stop(recognition.removeEventListener('end', escreve()))
+    : recognition.start(recognition.addEventListener('end', escreve()))
 
   button.innerHTML = listening ? 'Aperta para falar' : 'Para de escutar';
-
-  button.classList.toggle('bg-purple-200');
-  button.classList.toggle('bg-purple-500');
 });
 
 function creatRecognition() {
   const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-  const recognition = SpeechRecognition !== undefined ? new SpeechRecognition() : null
+  const recognition = SpeechRecognition !== undefined ? new SpeechRecognition() : null;
 
   if(!recognition) {
-    text.innerHTML = "Speech Recognition is not found!"
-    return null
+    text.innerHTML = "Speech Recognition is not found!";
+    return null;
   }
 
-  recognition.lang = "pt-BR"
+  recognition.lang = "pt_BR";
 
+  recognition.interimResults = true;
   recognition.onstart = () => listening = true;
   recognition.onend = () => listening = false;
   recognition.onerror = (e) => console.log('error', e); 
-  recognition.onresult = (e) => text.innerHTML = e.results[0][0].transcript;
-  // {  
-  //   for(let i = 0; i < e.results; i ++) {
-  //     if(e.results[i].isFinal) {
-  //       const fala = e.results[0][0].transcript;
-  //       text.value += fala;
-  //     }
-  //   }
-  // }
+  recognition.onresult = (e) => text2.innerHTML = e.results[0][0].transcript;
 
   return recognition;
 }
